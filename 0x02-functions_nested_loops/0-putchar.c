4e4e09b64f19af3d2e7f183398ef1e5af34c7a4b
TsTsoundz <adacheprince@gmail.com>
2023-08-15 16:35:08 -0700

    Man

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#inlude "main.h"
+ /**
+  * the main -print_putchar, followed with by a new line
+  * Return: Alays 0 (successful)
+  */
+int main(void)
+{
+	putchar(");
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n);
+
+		return (0);
+}
